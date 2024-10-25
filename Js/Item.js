052825b97788f656d29260b9a962db1179719a92
dilmikavishka <dilmikavishka14@gmail.com>
2024-10-25 22:31:04 +0530

    save done customer and item

---
+++ Js/Item.js
@@ -0,0 +1,48 @@
+let item_arr = [];
+
+function generateId() {
+    return item_arr.length + 1;
+}
+
+const loadItems = () => {
+    $("#item-tbody").empty();
+    item_arr.map((item, index) => {
+        let data = `<tr data-id="${index}">
+                        <td>${item.itemId}</td>
+                        <td>${item.itemName}</td>
+                        <td>${item.itemQty}</td>
+                        <td>${item.itemPrice}</td>
+                        <td><button class="btn btn-danger delete-btn" data-id="${index}">Delete</button></td>
+                    </tr>`;
+        $("#item-tbody").append(data);
+
+    });
+
+}
+
+function clearfiled() {
+    console.log("clearfiled");
+    $('#Item_Name').val('');
+    $('#Item_qty').val('');
+    $('#price').val('');
+}
+
+
+$("#item_save_button").on("click", function () {
+    let itemId = generateId();
+    let itemName = $("#Item_Name").val();
+    let itemQty = $("#Item_qty").val();
+    let itemPrice = $("#price").val();
+
+    let item = {
+        itemId: itemId,
+        itemName: itemName,
+        itemQty: itemQty,
+        itemPrice: itemPrice
+    };
+
+    item_arr.push(item);
+    loadItems();
+    clearfiled();
+});
+
